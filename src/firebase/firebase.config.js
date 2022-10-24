7d5aa5c14a7957ed31dad5b52c8a360c327c1b41
sufian <sufian4se@gmail.com>
2022-10-24 21:04:02 +0600

    install firebase, react-router-dom, react-icon

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBgCOsqcCAGvQ63_gQI_tXOoJs4d7pzSoE",
+  authDomain: "e-school-bc1e7.firebaseapp.com",
+  projectId: "e-school-bc1e7",
+  storageBucket: "e-school-bc1e7.appspot.com",
+  messagingSenderId: "340623420188",
+  appId: "1:340623420188:web:63875a876320f6c87f68d9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
