dacaaac329289bb14546ea8bdd9c12cdc4913dd0
sufian <sufian4se@gmail.com>
2022-10-25 19:37:22 +0600

    login page created

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <div className="formInner">
+          <div className="singleInput">
+            <label htmlFor="">Your Email</label>
+            <input type="email" name="email" placeholder="Your Email" />
+          </div>
+          <div className="singleInput">
+            <label htmlFor="">Password</label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Enter Password"
+            />
+          </div>
+        </div>
+        <button type="submit" className="btn-submit">
+          Login
+        </button>
+        <p className="infoText">
+          Already have an account? <Link to="/register">Register</Link>
+        </p>
+      </form>
     </div>
   );
 };
