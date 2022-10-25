481039d23e8e11a1a5bff48dcd67a9312d617508
sufian <sufian4se@gmail.com>
2022-10-25 19:28:46 +0600

    register functionality added

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../../context/AuthProvider/AuthProvider";
 import "./Register.css";
 
 const Register = () => {
+  const { createUser } = useContext(AuthContext);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const photoURL = form.image.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    // console.log(name, photoURL, email, password);
+
+    createUser(email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        form.reset();
+      })
+      .then((error) => {
+        console.error(error);
+      });
+  };
+
   return (
     <div className="container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="formInner">
           <div className="singleInput">
             <label htmlFor="">Your Name</label>
@@ -28,8 +51,10 @@ const Register = () => {
             />
           </div>
         </div>
-        <button className="btn-submit">Register</button>
-        <p className="infoText">Already have an account? <Link to='/login'>Login</Link></p>
+        <button type="submit" className="btn-submit">Register</button>
+        <p className="infoText">
+          Already have an account? <Link to="/login">Login</Link>
+        </p>
       </form>
     </div>
   );
