301b4c2b4f27ab09f9113c762c32358b1c99d1c5
sufian <sufian4se@gmail.com>
2022-10-25 20:24:25 +0600

    logout functionality added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { FaLaptop } from "react-icons/fa";
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../context/AuthProvider/AuthProvider';
 import './Header.css';
 
 const Header = () => {
+  const {user, logOut} = useContext(AuthContext);
+
+  const handleLogOut = () => {
+    logOut()
+    .then(() => { })
+    .catch(error => console.error(error))
+  }
+
   return (
     <nav className='nav'>
       <div className="mainNav">
@@ -16,6 +25,22 @@ const Header = () => {
             <li><Link to='/courses'>Courses</Link></li>
             <li><Link to='/faq'>FAQ</Link></li>
             <li><Link to='/blog'>Blog</Link></li>
+            <li>
+              <>
+              {
+                user?.uid ? 
+                <>
+                <span>{user?.displayName}</span>
+                  <button onClick={handleLogOut} className='btn-logout'>Log out</button>
+                </>
+                :
+                <div className='loginRegister'>
+                <Link to='/login'>Login</Link>
+                <Link to='/register'>Register</Link>
+                </div>
+              }
+              </>
+            </li>
           </ul>
         </div>
       </div>
--- src/context/AuthProvider/AuthProvider.js
+++ src/context/AuthProvider/AuthProvider.js
@@ -1,15 +1,19 @@
 import {
   createUserWithEmailAndPassword,
   getAuth,
-  signInWithEmailAndPassword
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut
 } from "firebase/auth";
-import React, { createContext } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import app from "../../firebase/firebase.config";
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
   const createUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
@@ -18,9 +22,22 @@ const AuthProvider = ({ children }) => {
     return signInWithEmailAndPassword(auth, email, password);
   };
 
+  const logOut = () => {
+    return signOut(auth);
+  }
+
+  useEffect( () => {
+    onAuthStateChanged(auth, (currentUser) => {
+      console.log('inside auth state change', currentUser);
+      setUser(currentUser);
+    })
+  }, [])
+
   const authInfo = {
+    user,
     createUser,
     signIn,
+    logOut
   };
 
   return (
